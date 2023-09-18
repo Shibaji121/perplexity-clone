ae6fdc2ad02774e0478575ddd043f383beb634d0
shiba_1234 <shibajisahu121@gmail.com>
2023-09-19 00:17:13 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "./Sidebar.js";
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../static/logo.svg";
+
+export default function SideBar() {
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  );
+}
