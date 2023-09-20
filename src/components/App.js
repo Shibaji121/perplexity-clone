2e6b2265bc5039f95ccc433fc1845686ccddd1f8
shiba_1234 <shibajisahu121@gmail.com>
2023-09-21 02:06:50 +0530

    router dom issue fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./HomePage.js";
 import SideBar from "./Sidebar.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SideBar />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <SideBar />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,79 +9,83 @@ import { ReactComponent as Arrow } from "../static/uparrow.svg";
 import { ReactComponent as Mobile } from "../static/mobile.svg";
 import { ReactComponent as Xtwiter } from "../static/x-twitter.svg";
 import { ReactComponent as Discord } from "../static/discord.svg";
+import { Link, Outlet } from "react-router-dom";
 
 export default function SideBar() {
   return (
-    <div className="sider-bar">
-      <div className="top-sideBar">
-        <div className="logo">
-          <a href="/">
-            <Logo />
-          </a>
+    <>
+      <div className="sider-bar">
+        <div className="top-sideBar">
+          <div className="logo">
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
+          <div className="new-thread flex-row">
+            <div>New Thread</div>
+            <div className="btn">Ctrl</div>
+            <div className="btn">I</div>
+          </div>
+          <div className="nav-options">
+            <div className="option">
+              <Link to="/">
+                <SearchIcon className="icon" />
+                Home
+              </Link>
+            </div>
+            <div className="option">
+              <Link to="/">
+                <Discover className="icon" />
+                Discover
+              </Link>
+            </div>
+            <div className="option">
+              <Link to="/">
+                <Library className="icon" />
+                Library
+                <div className="new">NEW</div>
+              </Link>
+            </div>
+            <div className="option">
+              <Link to="/">
+                <Login className="icon" />
+                Login
+              </Link>
+            </div>
+          </div>
+          <button>Sign Up</button>
         </div>
-        <div className="new-thread flex-row">
-          <div>New Thread</div>
-          <div className="btn">Ctrl</div>
-          <div className="btn">I</div>
-        </div>
-        <div className="nav-options">
-          <div className="option">
-            <a href="/">
-              <SearchIcon className="icon" />
-              Home
-            </a>
+        <div className="bottom-sideBar">
+          <div style={{ padding: "0px 16px 8px" }}>
+            <div style={{ color: "#1e3d44" }}>Try Pro</div>
+            <div>
+              Upgrade to Claude-2 or GPT-4, boost your Copilot uses, and upload
+              more files.
+            </div>
+            <div>
+              <div className="flex-row learn-more">
+                <Arrow className="icon" />
+                Learn More
+              </div>
+            </div>
           </div>
-          <div className="option">
-            <a href="/">
-              <Discover className="icon" />
-              Discover
-            </a>
-          </div>
-          <div className="option">
-            <a href="/">
-              <Library className="icon" />
-              Library
-              <div className="new">NEW</div>
-            </a>
-          </div>
-          <div className="option">
-            <a href="/">
-              <Login className="icon" />
-              Login
-            </a>
-          </div>
-        </div>
-        <button>Sign Up</button>
-      </div>
-      <div className="bottom-sideBar">
-        <div style={{ padding: "0px 16px 8px" }}>
-          <div style={{ color: "#1e3d44" }}>Try Pro</div>
-          <div>
-            Upgrade to Claude-2 or GPT-4, boost your Copilot uses, and upload
-            more files.
-          </div>
-          <div>
-            <div className="flex-row learn-more">
-              <Arrow className="icon" />
-              Learn More
+          <div className="flex-row download">
+            <div>
+              <Link to="/">
+                <Mobile className="icon" />
+                Download
+              </Link>
+            </div>
+            <div>
+              <Xtwiter className="icon" />
+            </div>
+            <div>
+              <Discord className="icon" />
             </div>
           </div>
         </div>
-        <div className="flex-row download">
-          <div>
-            <a href="/">
-              <Mobile className="icon" />
-              Download
-            </a>
-          </div>
-          <div>
-            <Xtwiter className="icon" />
-          </div>
-          <div>
-            <Discord className="icon" />
-          </div>
-        </div>
       </div>
-    </div>
+      <Outlet />
+    </>
   );
 }
